e6c7c21ee9fdaf3398cf8f9e2eaf06c34b99a089
Sarunyoo-dm <sarunyoochimmalee@gmail.com>
2023-01-04 17:50:03 +0700

    first commit

---
+++ order.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName:'Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName:'Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName:'Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName:'Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName:'Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Declined'
+    },
+    {
+        productName:'Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+]
